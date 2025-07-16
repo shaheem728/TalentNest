58b90d064140b9a7ae703c3d286d558c8e52f4fb
shaheem <shaheemmahmood728@gmail.com>
2025-07-16 12:53:59 +0530

    Added search functionality to Hero component and AppContext

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -2,6 +2,7 @@ import { createContext } from "react";
 
 const AppContext = createContext()
 export const AppContextProvider = (props) => {
+
     return (
     <AppContext.Provider value={{}}>
      {props.children}
