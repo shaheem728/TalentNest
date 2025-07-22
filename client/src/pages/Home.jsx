10c6faec81b4c441bae4197ed232d6f31cfc7c6a
shaheem <shaheemmahmood728@gmail.com>
2025-07-22 18:13:53 +0530

    Added AppDownload component to Home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import Hero from '../components/Hero'
 import JobListing from '../components/JobListing'
+import AppDownload from '../components/AppDownload'
 const Home = () => {
   return (
     <div>
       <Navbar />
       <Hero/>
       <JobListing/>
+      <AppDownload/>
     </div>
   )
 }
